aa5afdc64f4b110cc3f113c6f6d3a762570cb487
Paul Taylor <pltorigami@gmail.com>
2020-05-27 13:34:19 -0400

    badfollowers.py works for some accounts, returns stale element error for following list sometimes

--- badfollowers.py
+++ badfollowers.py
@@ -3,61 +3,108 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.events import EventFiringWebDriver, AbstractEventListener
 
 from secrets import *
 
-browser = webdriver.Chrome()
-browser.get('https://www.instagram.com')
 
-form = WebDriverWait(browser, 10).until(EC.element_to_be_clickable(
-        (By.TAG_NAME, 'form')))
-
-formlist = form.find_elements_by_tag_name('input')
-
-for char in iguser:  # using both for loops seems to avoid having to click 'new posts'
-    formlist[0].send_keys(char)
-
-for char in igpwd:
-    formlist[1].send_keys(char)
-
-formlist[1].send_keys(Keys.RETURN)
-
-notnow = WebDriverWait(browser, 10).until(EC.element_to_be_clickable(
-        (By.CLASS_NAME, 'HoLwm')))
-
-notnow.click()
-
-myaccount = browser.find_element_by_xpath('//*[@id="react-root"]/section/main/section/div[3]/div[1]/div/div[1]/a')
-myaccount.click()
-
-cssselector = 'a[href="/' + iguser + '/followers/"]'
-
-myfollowers = WebDriverWait(browser, 10).until(EC.element_to_be_clickable(
-        (By.CSS_SELECTOR, cssselector)))
-
-numberoffollowers = int(myfollowers.find_element_by_tag_name('span').text)
-
-myfollowers.click()
-
-followerbox = WebDriverWait(browser, 10).until(EC.element_to_be_clickable(
+def getlist(driver, totalnum, type):
+    box = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
         (By.CSS_SELECTOR, 'ul[class="jSC57  _6xe7A"]')))
 
-followerlist = []
+    print(f'Scraping Instagram page for {type}...')
 
-ef_driver = EventFiringWebDriver(browser, AbstractEventListener())
+    rawlist = []
 
-while len(followerlist) != numberoffollowers:
-    ef_driver.execute_script("document.querySelector('div[class=\"isgrP\"]').scrollTop += 1000")
-    followerlist = followerbox.find_elements_by_tag_name('li')
+    while len(rawlist) != totalnum:
+        driver.execute_script("document.querySelector('div[class=\"isgrP\"]').scrollTop += 1000")
+        rawlist = box.find_elements_by_tag_name('li')
 
-nameandhandlelist = []
+    print(f'Parsing {type} list...')
 
-for person in followerlist:
-    allinfo = person.text.split()
-    name = ' '.join(allinfo[1:-1])
-    handle = allinfo[0]
-    nameandhandlelist.append((name, handle))
+    if type == 'followers':
+        with open('followerserror.txt', 'w') as f:
+            print(rawlist, file=f)
+    else:
+        with open('followingerror.txt', 'w') as f:
+            print(rawlist, file=f)
 
-print(nameandhandlelist)
-print(len(nameandhandlelist))
+    finallist = []
+
+    for person in rawlist:
+        allinfo = person.text.split()
+        name = ' '.join(allinfo[1:-1])
+        handle = allinfo[0]
+        finallist.append((name, handle))
+
+    print()
+
+    return finallist
+
+
+def main():
+    print()
+
+    browser = webdriver.Chrome()
+    browser.get('https://www.instagram.com')
+
+    form = WebDriverWait(browser, 10).until(EC.element_to_be_clickable(
+            (By.TAG_NAME, 'form')))
+
+    formlist = form.find_elements_by_tag_name('input')
+
+    for char in iguser:  # using both for loops seems to avoid having to click 'new posts'
+        formlist[0].send_keys(char)
+
+    for char in igpwd:
+        formlist[1].send_keys(char)
+
+    formlist[1].send_keys(Keys.RETURN)
+
+    notnow = WebDriverWait(browser, 10).until(EC.element_to_be_clickable(
+            (By.CLASS_NAME, 'HoLwm')))
+
+    notnow.click()
+
+    myaccount = browser.find_element_by_xpath('//*[@id="react-root"]/section/main/section/div[3]/div[1]/div/div[1]/a')
+    myaccount.click()
+
+    cssselector = 'a[href="/' + iguser + '/followers/"]'
+
+    myfollowers = WebDriverWait(browser, 10).until(EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, cssselector)))
+
+    numberoffollowers = int(myfollowers.find_element_by_tag_name('span').text)
+
+    myfollowers.click()
+
+    followerlist = getlist(browser, numberoffollowers, 'followers')
+
+    browser.back()
+
+    newcssselector = 'a[href="/' + iguser + '/following/"]'
+
+    myfollowing = browser.find_element_by_css_selector(newcssselector)
+
+    numberoffollowing = int(myfollowing.find_element_by_tag_name('span').text)
+
+    myfollowing.click()
+
+    followinglist = getlist(browser, numberoffollowing, 'following')
+
+    browser.quit()
+
+    badfollowerlist = [tup for tup in followinglist if tup not in followerlist]
+
+    badfollowerlist.sort(key=lambda x: x[1])
+
+    with open('badfollowers.txt', 'w') as endfile:
+        print(f'Peolpe who @{iguser} follows but who did not return the favor:\n\n', file=endfile)
+        print('Instagram handle  :  name\n', file=endfile)
+
+        for name, handle in badfollowerlist:
+            print(f'{handle}  :  {name}', file=endfile)
+
+    print('Done. Look for a "badfollowers.txt" file in your current directory.')
+
+
+main()
